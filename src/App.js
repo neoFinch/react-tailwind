856e81af8d7cd237188ebf047b3c37a7cfb01d69
Vaibhav Sharma <vaibhav.sharma@qed42.com>
2020-09-13 22:16:38 +0530

    initial commit -m

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center h-screen">
+      <div className='p-2 self-center'>
+        <h1 className='text-5xl'>ReactJS with Tailwind CSS</h1>
+      </div>
     </div>
   );
 }
